f6394ea6e4acf96be564be1149191c69e87e2a92
nickpiercy95 <48226990+nickpiercy95@users.noreply.github.com>
2019-11-06 10:45:36 +1300

    Added Powershell profile

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,20 @@
+# If we're in system32, change to $env:HOME
+$CurrentDir = $(Get-Location).Path
+if ($CurrentDir -eq "C:\WINDOWS\system32") {
+    Set-Location $env:HOME
+}
+
+Set-Alias ifconfig ipconfig
+
+function xx {exit}
+
+# Import Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module Posh-Git
+
+# shell-style tab completion
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
