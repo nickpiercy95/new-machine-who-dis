e53a4840658f49c6cb759ed73fed2d5c1546442e
Nick Piercy <nick.piercy@nzx.com>
2020-06-02 11:07:37 +1200

    Added GIT_SSH_COMMAND to Powershell profile

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -4,6 +4,9 @@ if ($CurrentDir -eq "C:\WINDOWS\system32") {
     Set-Location $HOME
 }
 
+# Tell Git to use the SSH library included in Windows 10, rather than its own. See https://www.teapotcoder.com/post/how-to-fix-git-ssh-asking-for-password-on-windows-10/
+$env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace('\' ,'/')
+
 Set-Alias ifconfig ipconfig
 
 function xx {exit}
