bc5e8f2106e38c6aee3eaeb523add0880dfe07f1
Nick Piercy <nick.piercy@nzx.com>
2020-06-12 12:23:24 +1200

    Updated Powershell profile

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,13 +1,13 @@
-# If we're in system32, change to $HOME
-$CurrentDir = (Get-Location).Path
-if ($CurrentDir -eq "C:\WINDOWS\system32") {
+if ((Get-Location).Path -eq "C:\WINDOWS\system32") {
     Set-Location $HOME
 }
 
-# Tell Git to use the SSH library included in Windows 10, rather than its own. See https://www.teapotcoder.com/post/how-to-fix-git-ssh-asking-for-password-on-windows-10/
+# Tell Git to use the SSH library included in Windows 10 instead of its own, to avoid ssh-agent shenanigans.
+# See https://www.teapotcoder.com/post/how-to-fix-git-ssh-asking-for-password-on-windows-10/
 $env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace('\' ,'/')
 
 Set-Alias ifconfig ipconfig
+Set-Alias vim "C:\Program Files\Git\usr\bin\vim.exe"
 
 function xx {exit}
 
