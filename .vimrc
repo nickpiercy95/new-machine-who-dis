331e542308360b8456888a0bd81182aecfc8b243
Nick Piercy <nick.piercy@nzx.com>
2020-08-06 16:43:00 +1200

    Updated settings

--- .vimrc
+++ .vimrc
@@ -1,7 +1,8 @@
 set tabstop=4
 set shiftwidth=4
 set expandtab
-set vb
 set backspace=indent,eol,start
+set visualbell
+set t_vb=
 
 colorscheme desert
--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -3,18 +3,19 @@ if ((Get-Location).Path -eq "C:\WINDOWS\system32") {
 }
 
 # Tell Git to use the SSH library included in Windows 10 instead of its own, to avoid ssh-agent shenanigans.
-# See https://www.teapotcoder.com/post/how-to-fix-git-ssh-asking-for-password-on-windows-10/
-$env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace('\' ,'/')
+                                                                                            
+$env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace("\" ,"/")
 
 Set-Alias ifconfig ipconfig
 Set-Alias host Resolve-DnsName
+Remove-Item alias:diff -Force
 
 function xx {exit}
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
+if (Test-Path "$ChocolateyProfile") {
+    Import-Module "$ChocolateyProfile"
 }
 
 Import-Module Posh-Git
@@ -24,4 +25,3 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 # Disable bell
 Set-PSReadlineOption -BellStyle None
-
