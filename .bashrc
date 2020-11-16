eff396532311dc288186fcc4debacb8f1966c4ed
Nick Piercy <nick.piercy@nzx.com>
2020-11-17 09:26:31 +1300

    Updated settings from work laptop

--- .bashrc
+++ .bashrc
@@ -1,6 +1,7 @@
 alias ls='ls -lhAB --color=auto'
 alias mv='mv -v'
 alias cp='cp -v'
+alias rm='rm -v'
 alias grep='grep --color=always'
 alias python='winpty python'
 
@@ -11,3 +12,7 @@ function start-ssh-agent {
 
 # Trim directory path in prompt
 export PROMPT_DIRTRIM=2
+
+export HOSTS=/c/Windows/System32/drivers/etc/hosts
+export TSMR=/c/Windows/tsmr_NZX.ini
+
--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -2,14 +2,13 @@ if ((Get-Location).Path -eq "C:\WINDOWS\system32") {
     Set-Location $HOME
 }
 
-# Tell Git to use the SSH library included in Windows 10 instead of its own, to avoid ssh-agent shenanigans.
-$env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace("\" ,"/")
+# Tell Git to use the Windows SSH library instead of its own, to avoid ssh-agent shenanigans
+$env:GIT_SSH_COMMAND = (Get-Command ssh).Source.Replace('\' ,'/')
 
 Set-Alias ifconfig ipconfig
 Set-Alias host Resolve-DnsName
-Remove-Item alias:diff -Force
-
-function xx {exit}
+Set-Alias gitbash "C:\Program Files\Git\bin\bash.exe"
+Remove-Item alias:diff -Force > $null 2>&1
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -18,9 +17,14 @@ if (Test-Path "$ChocolateyProfile") {
 }
 
 Import-Module Posh-Git
+$GitPromptSettings.DefaultForegroundColor = [ConsoleColor]::DarkGreen
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+$GitPromptSettings.DefaultPromptPrefix = "$(whoami) "
 
-# Unix-style tab completion
+# Unix-style shortcuts
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
-# Disable bell
 Set-PSReadlineOption -BellStyle None
+
+$HOSTS = "C:\Windows\System32\drivers\etc\hosts"
